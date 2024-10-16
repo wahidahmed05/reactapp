a733de42612983e8b7566444cf398be43d7145f9
wahidahmed05 <wahidahmed111002@gmail.com>
2024-10-16 11:23:33 +0600

    Updated feature NavCom Components for nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import NavCom from "./components/NavCom"
 
 function App() {
 
   return (
     <>
 <h1>HELLO REACT APPP</h1>
+<NavCom />
     </>
   )
 }
---
+++ src/components/NavCom.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+const NavCom =()=>{
+    return <>
+    <a href="">Heloo </a>
+    <a href="">Heloo </a>
+    <a href="">Heloo </a>
+    <a href="">Heloo </a>
+    <a href="">Heloo </a>
+    <a href="">Heloo </a>
+    <a href="">Heloo </a>
+    
+    </>
+}
+
+
+export default NavCom
